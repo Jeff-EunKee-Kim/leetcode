b11c8dd81f19dc79eb7ab53da15d199a1017777d
Jeff Kim <ek111@duke.edu>
2019-02-25 16:58:50 -0500

    more

--- Easy/disappeared_number.java
+++ Easy/disappeared_number.java
@@ -1,5 +1,13 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        
+        List<Integer> ans = new ArrayList<>();
+        HashSet<Integer> withoutDup = new HashSet<Integer>(); 
+        for(int i : nums){
+            withoutDup.add(i);
+        }
+        for(int i = 1; i <= nums.length; i ++){
+            if(!withoutDup.contains(i)) ans.add(i);
+        }
+        return ans; 
     }
 }
--- Medium/subsets2.java
+++ Medium/subsets2.java
@@ -1,9 +1,9 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>(); 
         
         if(nums.length == 0 || nums == null) return ans; 
-        
+        Arrays.sort(nums); 
         List<Integer> runner = new ArrayList<Integer>(); 
         
         helper(ans, runner, 0, nums);
@@ -14,6 +14,7 @@ class Solution {
         ans.add(new ArrayList<>(runner));
         
         for (int i = index; i < nums.length; i ++){
+            if(i > index && nums[i] == nums[i-1]) continue;
             runner.add(nums[i]); 
             helper(ans, runner, i + 1, nums); 
             runner.remove(runner.size() -1 ); 
