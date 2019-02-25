f96173da1b8c55e48a79a22ee59cc3f6fc92dc47
Jeff Kim <ek111@duke.edu>
2019-02-25 13:41:28 -0500

    code

--- Easy/majority_element.java
+++ Easy/majority_element.java
@@ -1,5 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        
+        if (nums.length == 1 ) return nums[0];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int lim = n/2; 
+
+        for(int i : nums){
+            map.put(i, map.getOrDefault(i, 0) + 1); 
+            if(map.get(i) > lim) return i ;
+        }
+        return 0;
+
     }
 }
