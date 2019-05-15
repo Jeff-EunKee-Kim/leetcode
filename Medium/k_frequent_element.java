0630e2cfa77416ce591394467f89cca208635a77
Jeff Kim <eunkeekim@gmail.com>
2019-05-15 15:12:42 +0900

    d

--- Medium/k_frequent_element.java
+++ Medium/k_frequent_element.java
@@ -4,10 +4,15 @@ class Solution {
         for (int i = 0; i< nums.length; i++){
             map.getOrDefault(nums[i]+1, 1);
         }
-        int ans = new int[k];
-        int count = 0;
-        while (k > 0){
-            
+        PriorityQueue<Integer> queue = new PriorityQueue<>(); 
+        for(int key : map.keySet){
+            queue.add(map.get(key)); 
         }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(k > 0){
+            ans.add(queue.poll);
+            k--;
+        }
+        return ans;
     }
 }
